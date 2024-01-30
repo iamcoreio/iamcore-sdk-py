e16c70319b1f5af3045ca8f7a5c5b53b289e4280
Aleksandr Burdeinyi <aburdeinyi@kaaiot.io>
2024-01-30 18:00:07 +0000

    Agile 0000

--- iamcore/client/evaluete.py
+++ iamcore/client/evaluete.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Generator, Union, Any
 
 import requests
@@ -7,6 +8,8 @@ from requests import Response
 from iamcore.client.common import IamEntitiesResponse, generic_search_all
 from iamcore.client.conf import IAMCORE_URL
 from iamcore.client.exceptions import EVALUATE_MAPPING, unwrap_return_empty, IAMException, unwrap_return_json
+
+logger = logging.getLogger(__name__)
 
 
 def authorize(
@@ -18,7 +21,7 @@ def authorize(
         resource_type: str,
         resource_path: str,
         action: str,
-        resource_ids: Union[List[str], None] = None) -> Generator[str, Any, None]:
+        resource_ids: Union[List[str], None] = None) -> List[IRN]:
     if not action:
         raise IAMException("Action must be defined")
     tenant_id = tenant_id or principal_irn.tenant_id
@@ -34,12 +37,11 @@ def authorize(
                 resource_id=resource_id)
             for resource_id in resource_ids
         ]
+        logger.debug(f"Going to evaluate {authorization_headers} {action} {resources_irn_list}")
         evaluate(authorization_headers, action, resources_irn_list)
-        return (r for r in resource_ids)
-    return (
-        irn.resource_id
-        for irn in evaluate_all_resources(authorization_headers, application, action, resource_type)
-    )
+        return resources_irn_list
+    logger.debug("Going to evaluate by type")
+    return list(evaluate_all_resources(authorization_headers, application, action, resource_type))
 
 
 def evaluate(auth_headers: dict[str, str], action: str, resources: List[IRN]) -> None:
@@ -52,6 +54,7 @@ def evaluate(auth_headers: dict[str, str], action: str, resources: List[IRN]) ->
         "Content-Type": "application/json",
         **auth_headers
     }
+    logger.debug(f"Going to evaluate resources: POST {url}, json={payload}")
     response: Response = requests.request("POST", url, json=payload, headers=headers)
     return unwrap_return_empty(response, EVALUATE_MAPPING)
 
@@ -77,6 +80,7 @@ def evaluate_resources(
         "Content-Type": "application/json",
         **auth_headers
     }
+    logger.debug(f"Going to evaluate resource type: POST {url}, json={payload}, params={querystring}")
     response: Response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
     return IamEntitiesResponse(IRN, **unwrap_return_json(response, EVALUATE_MAPPING))
 
