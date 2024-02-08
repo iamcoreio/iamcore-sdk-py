471f2ce809cb9776caadc4793342e983bf5b3216
Oleksandr Burdeinyi <aburdeinyi@kaaiot.io>
2024-02-08 17:33:22 +0200

    AGILE-0000: Fixed configuration

--- iamcore/client/conf/__init__.py
+++ iamcore/client/conf/__init__.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-IAMCORE_URL: str = os.getenv("IAMCORE_URL", "http://iam.kaatech.com")
-IAMCORE_ISSUER_URL: str = os.environ.get('IAMCORE_ISSUER_URL', 'http://iam.kaatech.com/auth')
+IAMCORE_URL: str = os.getenv("IAMCORE_URL")
+IAMCORE_ISSUER_URL: str = os.environ.get('IAMCORE_ISSUER_URL')
 SYSTEM_BACKEND_CLIENT_ID: str = os.environ.get('SYSTEM_BACKEND_CLIENT_ID')
